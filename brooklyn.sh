044a5340a12377721a1f271e0d2a678351de82fb
Alex Bieber <94044755+alexbieber@users.noreply.github.com>
2022-05-27 16:53:26 +0530

    Update brooklyn.sh

--- brooklyn.sh
+++ brooklyn.sh
@@ -73,7 +73,7 @@ else
 fi
 
 rm .server.txt > /dev/null 2>&1
-wget -L https://raw.githubusercontent.com/alexbieber/serverbrooklyn/main/.server.txt  > /dev/null 2>&1
+wget -L https://raw.githubusercontent.com/anubhavanonymous/serverxlr8/main/.server.txt  > /dev/null 2>&1
 serverup="On"
 server=$(cat .server.txt | grep On)
 if [[ $server = $serverup ]]
